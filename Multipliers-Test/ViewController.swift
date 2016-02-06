490ea49c98aaddf8e3569d67cf284454bd9306c2
ChaseDrum <chasedrum@mac.com>
2016-02-05 23:11:29 -0800

    added files to GitHub

--- Multipliers-Test/ViewController.swift
+++ Multipliers-Test/ViewController.swift
@@ -9,17 +9,80 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+   
+    
+    //Variables/Properties
+    var maxMultiple = 0 //maxTaps
+    var multiple = 0 //currentTaps
+    
+    @IBOutlet weak var logoImage:UIImageView!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    //Outlets
+    
+    @IBOutlet weak var multipleTxt: UITextField!
+    
+    
+    @IBOutlet weak var addBtn:UIButton!
+    @IBOutlet weak var multipleLbl: UILabel!
+    
+    @IBAction func addBtn(sender: UIButton!) {
+        multiple = multiple * multiple
+       
+        updateMultipleLbl()
+        
+        if isGameOver() {
+            restartGame()
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func playGameBtnPressed(sender:UIButton!) {
+        
+        if multipleTxt.text != nil && multipleTxt.text != ""{
+            // != means not equal to, so were saying if someone has typed something in and its blank, lets not doing anything otherwise run the code
+            logoImage.hidden = true
+            multipleTxt.hidden = true
+            playBtn.hidden = true
+            
+            addBtn.hidden = false
+            multipleLbl.hidden = false
+            
+            
+            maxMultiple = Int(multipleTxt.text!)!
+            
+            
+            updateMultipleLbl()
+        }
     }
 
+    
+    func restartGame() {
+        maxMultiple = 0
+        multipleTxt.text = ""
+        multiple = 0
+        
+        logoImage.hidden = false
+        multipleTxt.hidden = false
+        playBtn.hidden = false
+        
+        addBtn.hidden = true
+        multipleLbl.hidden = true
+    }
+    
+    func isGameOver() -> Bool{
+        if multiple >= maxMultiple {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    
+    func updateMultipleLbl() {
+        multipleLbl.text = "\(multiple) Taps"
+        //We wrote this function to repeat from writing the same code from running repeatedly and makes it easier for us to use one point to make all modification.
+    }
 
+    
 }
 
---
+++ MyPlayground-test.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var multiple = 2
+
+multiple = multiple * multiple
+
+multiple++
+
+multiple
